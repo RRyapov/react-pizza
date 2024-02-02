0554753df73ba04cc7a56bcc77777142647c0c08
RRyapov <rryapov@gmail.com>
2024-02-02 11:03:54 +0300

    Данные JSON-сервера подтягиваются по 3000 порту

--- src/redux/actions/actionPizzas.js
+++ src/redux/actions/actionPizzas.js
@@ -9,7 +9,7 @@ export const fetchPizzas = (activeSortBy, category) => (dispatch) => {
   dispatch(setLoaded(false));
   axios
     .get(
-      `http://localhost:3001/pizzas?${
+      `pizzas?${
         category !== null ? `category=${category}` : ""
       }&_sort=${activeSortBy}`
     )
