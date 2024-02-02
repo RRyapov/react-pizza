015e1e0a763357bc0e8e8e891654ca5fe6bf7ecd
RRyapov <rryapov@gmail.com>
2024-02-02 17:35:22 +0300

    Добавлено очищение корзины по кнопке, а также страницу пустой корзины

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -3,19 +3,27 @@ const initialState = {
   totalPrice: 0,
   totalCount: 0,
 };
+
+const getTotalPrice = (arr) => arr.reduce((sum, obj) => obj.price + sum, 0);
 // это редюсер фильтрации
 const cart = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_PIZZA_CART": {
+      const currentPizzaItems = !state.items[action.payload.id]
+        ? [action.payload]
+        : [...state.items[action.payload.id].items, action.payload];
+
       const newItems = {
         ...state.items,
-        [action.payload.id]: !state.items[action.payload.id]
-          ? [action.payload]
-          : [...state.items[action.payload.id], action.payload],
+        [action.payload.id]: {
+          items: currentPizzaItems,
+          totalPrice: getTotalPrice(currentPizzaItems),
+        },
       };
 
-      const allPizzas = [].concat.apply([], Object.values(newItems));
-      const allPizzasPrice = allPizzas.reduce((sum, obj) => obj.price + sum, 0);
+      const items = Object.values(newItems).map((obj) => obj.items);
+      const allPizzas = [].concat.apply([], items);
+      const allPizzasPrice = getTotalPrice(allPizzas);
 
       return {
         ...state,
@@ -29,6 +37,13 @@ const cart = (state = initialState, action) => {
     //         ...state,
     //         totalCount: action.payload,
     //       };
+
+    case "CLEAR_CART":
+      return {
+        items: {},
+        totalPrice: 0,
+        totalCount: 0,
+      };
 
     default:
       return state;
